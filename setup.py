226a34500d52dcef8c856f7bfb79a312bc13a184
Dror Kronstein <dkronst@gmail.com>
2013-05-09 00:59:30 +0300

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import os
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name='mexbtcapi',
+    version='0.1',
+    description="The VirCurEx Trade API",
+    long_description=read('README.md'),
+    author="Christopher Poole",
+    url='https://github.com/christopherpoole/pyvircurex.git',
+    license='CC0',
+    packages=find_packages(exclude=['ez_setup']),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries',
+    ],
+)
