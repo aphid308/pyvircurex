d46f551149c300493d821afd46c9595754985113
Jaka Hudoklin <jakahudoklin@gmail.com>
2013-05-25 15:52:50 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='vircurex',
+      version='0.1',
+      description='Python wrapper for the Vircurex cryptocurrency exchange API',
+      author='Cristopher Poole',
+      url='https://github.com/christopherpoole/pyvircurex',
+      packages=['vircurex'],
+     )
