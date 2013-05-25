08bff3971e486b3a25be273948eea3be1912478f
Christopher Poole <mail@christopherpoole.net>
2013-05-26 09:54:40 +1000

    added email address

--- setup.py
+++ setup.py
@@ -5,7 +5,8 @@ from distutils.core import setup
 setup(name='vircurex',
       version='0.1',
       description='Python wrapper for the Vircurex cryptocurrency exchange API',
-      author='Cristopher Poole',
+      author='Christopher Poole',
+      author_email='mail@christopherpoole.net',
       url='https://github.com/christopherpoole/pyvircurex',
       packages=['vircurex'],
      )
